59fbc29d6b4cabda64d0f92c1782c2a1aa54e1fd
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-10 22:51:03 -0300

    add files

--- project/settings.py
+++ project/settings.py
@@ -70,15 +70,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agenda',        # nome do seu banco
+        'USER': 'root',   # usuário do MySQL
+        'PASSWORD': 'Gabrielrochadias12', # senha do MySQL
+        'HOST': 'localhost',     # ou IP do servidor MySQL
+        'PORT': '3306',          # porta padrão do MySQL
     }
 }
+
 
 
 # Password validation
