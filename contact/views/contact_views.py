5c7a45c98b86e72537e1ccff00cf3db91e7a0b0e
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-13 20:40:17 -0300

    add files

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,7 +3,7 @@ from urllib import request
 from django.http import JsonResponse, response, HttpRequest, HttpResponse
 from typing import Any
 from django.shortcuts import render
-from . models import Contact
+from ..models import Contact
 
 # Create your views here.
 def index(request):
@@ -26,7 +26,7 @@ def users_view(request: HttpRequest) -> HttpResponse:
     print("Chegou aqui")
     return render(
         request,
-        'contact/index.html',
+        'contact/home.html',
     )
     return Users().return_all_users(request)
 
