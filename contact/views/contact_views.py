b38f385e7c4a7af1850328439a4995ced07d752c
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-15 19:21:05 -0300

    add files

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,10 @@
 from os import name
+from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+import contact
 from contact.admin import ContactAdmin
 from ..models import Contact
 
@@ -14,7 +16,7 @@ class Users:
     def __init__(self, param=None):
         self.all_users: list[dict[str, Any]] = list(Contact.objects.all().order_by('-id'))
         if param:
-            self.user: dict[str, Any] = Contact.objects.get(pk=param)
+            self.user: Contact = get_object_or_404(Contact.objects.filter(pk=param))
 
     def users_view(self, request: HttpRequest) -> HttpResponse:
         return render(request, "contact/home.html", context={"users": self.all_users})
