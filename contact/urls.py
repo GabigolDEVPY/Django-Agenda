b5557b3385d73140354dd52ce8e8286786c31e54
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-15 22:17:53 -0300

    add files

--- contact/urls.py
+++ contact/urls.py
@@ -1,12 +1,13 @@
+from re import search
 from django.contrib import admin
 from django.urls import path
 from contact import views
-import contact
 
 app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('users-json/', views.users_json, name='users'),
-    path('<int:id>/', views.user_detail, name='user')
+    path('<int:id>/', views.user_detail, name='user'),
+    path('search/', views.search, name='search')
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,7 +1,10 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q
 from django.http import HttpRequest, HttpResponse
+
 from typing import Any
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+
+import contact
 from ..models import Contact
 
 # Create your views here.
@@ -9,10 +12,12 @@ def index(request):
     return render(request,'contact/index.html')
     
 class Users:
-    def __init__(self, param=None):
-        self.all_users: list[Contact] = list(Contact.objects.all().order_by('-id'))
+    def __init__(self, param=None, search=None):
+        self.all_users: Contact = Contact.objects.all().order_by('-id')
         if param:
             self.user: Contact = get_object_or_404(Contact, pk=param)
+        elif search:
+            self.result_search: contact = Contact.objects.filter(Q(first_name__icontains=search) | Q(last_name__icontains=search))    
 
     def users_view(self, request: HttpRequest) -> HttpResponse:
         return render(request, "contact/home.html", context={"users": self.all_users})
@@ -20,10 +25,18 @@ class Users:
     def user_view(self, request: HttpRequest) -> HttpResponse:
         return render(request, "contact/contact.html", context={"contact": self.user})
     
-
+    def user_search(self, request: HttpRequest) -> HttpResponse:
+        return render(request, "contact/home.html", context={"users": self.result_search})
+    
 
 def users_json(request: HttpRequest) -> HttpResponse:
     return Users().users_view(request)
 
 def user_detail(request: HttpRequest, id: int) -> HttpResponse:
     return Users(id).user_view(request)
+
+def search(request: HttpRequest) -> HttpResponse:
+    value_search = request.GET.get("q").strip()
+    if value_search == '':
+        return redirect('contact:users')
+    return Users(search=value_search).user_search(request)
