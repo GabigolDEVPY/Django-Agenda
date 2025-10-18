30c6d10e9317a0c1ef55372d8978352580bd1222
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-10-18 09:57:12 -0300

    add files

--- contact/urls.py
+++ contact/urls.py
@@ -9,8 +9,9 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name="index"),
     path('users-json/', views.users_json, name='users'),
-    path('<int:id>/', views.user_detail, name='user'),
     path('search/', views.search, name='search'),
-    path('create/', views.create, name='create')
+    
+    path('contact/<int:id>/', views.user_detail, name='user'),
+    path('contact/create/', views.create, name='create')
     
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,11 +1,21 @@
 from django.db.models import QuerySet, Q
 from django.http import HttpRequest, HttpResponse
-from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
-import contact
-from ..models import Contact
+from contact.forms import ContactForm
+
 
 # Create your views here.
 def create(request):
-    return render(request,'contact/create.html')
+    if request.method == "POST":
+        print("request é post")
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("contact:create")
+        context = {'form': form}
+        return render(request,'contact/create.html', context)
+    
+    context = {'form': ContactForm()}
+    return render(request,'contact/create.html', context)
+    
     
